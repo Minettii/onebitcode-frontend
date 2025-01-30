bce97d726134ae1e502f6345f906bc6e65cbf6a0
YOUR_NAME <rafael_minetti@hotmail.com>
2025-01-30 12:45:30 -0300

    chore: eslint adjusment in components props

--- react/exercise2/src/components/Button/index.jsx
+++ react/exercise2/src/components/Button/index.jsx
@@ -1,5 +1,6 @@
 import styles from "./styles.module.scss";
 
 export default function Button(props) {
+	// eslint-disable-next-line react/prop-types
 	return <button className={styles.btnMain}>{props.text}</button>;
 }
--- react/exercise2/src/components/Card/index.jsx
+++ react/exercise2/src/components/Card/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styles from "./styles.module.scss";
 import Button from "../Button";
 
